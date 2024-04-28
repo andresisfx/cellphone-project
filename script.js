434c10fc2455ca789a1aac390af9bc03bb4d793c
andresisfx <124216395+andresisfx@users.noreply.github.com>
2024-04-27 22:28:20 -0500

    funtion sintaxis

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 "use strict";
-console.log("Hello world from typescript");
+const nacional = 5;
+const junior = 7;
+function jugar(equipo1, equipo2) {
+    if (equipo1 > equipo2) {
+        console.log(`${equipo1} ganó `);
+    }
+    if (equipo1 == equipo2) {
+        console.log(`${equipo1} y ${equipo2} empataron `);
+    }
+    if (equipo1 < equipo2) {
+        console.log(`${equipo2} ganó `);
+    }
+}
+jugar(nacional, junior);
--- script.ts
+++ script.ts
@@ -1,1 +1,10 @@
-console.log("Hello world from typescript")
+ const nacional :number =5;
+ const junior:number = 7;
+
+ function jugar (equipo1:number,equipo2:number){
+    if (equipo1>equipo2) {console.log(`${equipo1 } ganó `)}
+    if (equipo1== equipo2) {console.log(`${equipo1 } y ${equipo2} empataron `)}
+    if (equipo1<equipo2) {console.log(`${equipo2 } ganó `)}
+ }
+
+ jugar(nacional,junior)
